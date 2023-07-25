cf4a77cd8a901debaa5527ee6e903499521e35b7
elorahob <elorahob@uw.edu>
2023-07-24 20:12:16 -0700

    move header into shareable component folder

--- fit-as-a-fiddle/app/interactions/page.js
+++ fit-as-a-fiddle/app/interactions/page.js
@@ -1,12 +1,10 @@
 import styles from './page.module.css';
+import Header from '../components/shared/Header';
 
 export default function Interactions() {
   return (
     <main className={styles.main}>
-      <div className={`container-fluid text-center ${styles.hero}`}>
-        <h1>Drug Interaction Checker</h1>
-        <h2>Improve your safety and awareness by checking for possible conflicts between your medications.</h2>
-      </div>
+      <Header />
     </main>
   )
 }
