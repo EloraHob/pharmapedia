588288fa4aba40c81c18b31619120794ee36b1c6
haonanzzz <hz96@uw.edu>
2023-07-25 19:21:28 -0700

    layout change navbar

--- fit-as-a-fiddle/app/navbar.js
+++ fit-as-a-fiddle/app/navbar.js
@@ -1,32 +1,30 @@
 'use client'
 import React from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Image } from 'react-bootstrap';
+
 const MyNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#">
-        <Image src="/images/logo.png" alt="App Logo" width="30" height="30" className="d-inline-block align-top" /> {' '}
-        Pharmapedia
+      <Navbar.Brand href="#" className="ms-3">
+        <Image src="/images/logo.png" alt="App Logo" width="170" height="40" className="d-inline-block align-top" /> {' '}
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarSupportedContent" />
       <Navbar.Collapse id="navbarSupportedContent">
-        <Nav className="mr-auto">
-          <Nav.Link href="#" active>Home</Nav.Link>
-          <NavDropdown title="Features" id="navbarDropdown">
+        <Nav className="me-auto">
+          <Nav.Link href="#" className="ms-3 me-4" active>Home</Nav.Link>
+          <NavDropdown title="Features" id="navbarDropdown" className="me-4">
             <NavDropdown.Item href="#">Medication Information Filter</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#">Search</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#">Interaction checker</NavDropdown.Item>
           </NavDropdown>
-          <Nav.Link href="/about" active>About</Nav.Link>
+          <Nav.Link href="/about" className="me-4" active>About</Nav.Link>
         </Nav>
-        <div className="me-auto flex-row-reverse bd-highlight">
-          <Form inline className="d-flex my-2 my-lg-0">
-            <FormControl type="search" placeholder="Search" className="mr-sm-2 d-inline-block" aria-label="Search" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </div>
+        <Form className="d-flex ms-auto my-2 my-lg-0">
+          <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
