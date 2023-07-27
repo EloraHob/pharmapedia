0ec44a13b729922af2e5c4a34620a158bcff92a8
elorahob <elorahob@uw.edu>
2023-07-26 23:46:09 -0700

    update navbar paths

--- fit-as-a-fiddle/app/navbar.js
+++ fit-as-a-fiddle/app/navbar.js
@@ -5,7 +5,7 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Image } from 'reac
 const MyNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#" className="ms-3">
+      <Navbar.Brand href="/" className="ms-3">
         <Image src="./logo.png" alt="App Logo" width="170" height="40" className="d-inline-block align-top" /> {' '}
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarSupportedContent" />
@@ -17,7 +17,7 @@ const MyNavbar = () => {
             <NavDropdown.Divider />
             <NavDropdown.Item href="#">Search</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#">Interaction checker</NavDropdown.Item>
+            <NavDropdown.Item href="/interactions">Interaction checker</NavDropdown.Item>
           </NavDropdown>
           <Nav.Link href="/about" className="me-4" active>About</Nav.Link>
         </Nav>
