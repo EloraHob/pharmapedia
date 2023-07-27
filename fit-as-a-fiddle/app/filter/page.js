0324c781a386eaccc0bc385f8ceec0cb33ef5a6a
elorahob <elorahob@uw.edu>
2023-07-26 23:00:55 -0700

    Implement med cards display grid section

--- fit-as-a-fiddle/app/filter/page.js
+++ fit-as-a-fiddle/app/filter/page.js
@@ -2,7 +2,62 @@
 
 import styles from './page.module.css';
 import Header from '../components/Header';
-import MedicationInfoCard from './MedicationInfoCard';
+import ResultsDisplayGrid from './ResultsDisplayGrid';
+
+const medicationsData = [
+    [
+      { 
+        drugName: "Aspirin", 
+        activeIngredient: "Acetylsalicylic Acid", 
+        description: "Aspirin is a salicylate that is used to treat pain, and reduce fever or inflammation."
+      },
+      { 
+        drugName: "Tylenol", 
+        activeIngredient: "Acetaminophen", 
+        description: "Tylenol is used to treat mild to moderate and pain, to treat moderate to severe pain in conjunction with opiates, or to reduce fever." 
+      },
+      { 
+        drugName: "Advil", 
+        activeIngredient: "Ibuprofen", 
+        description: "Advil is a nonsteroidal anti-inflammatory drug (NSAID). It works by reducing hormones that cause inflammation and pain in the body." 
+      },
+    ],
+    [
+      { 
+        drugName: "Zyrtec", 
+        activeIngredient: "Cetirizine", 
+        description: "Zyrtec (Cetirizine) is an antihistamine used to relieve allergy symptoms such as watery eyes, runny nose, itching eyes/nose, sneezing, hives, and itching." 
+      },
+      { 
+        drugName: "Claritin", 
+        activeIngredient: "Loratadine", 
+        description: "Claritin (Loratadine) is an antihistamine used to relieve allergy symptoms such as watery eyes, runny nose, itching eyes/nose, and sneezing." 
+      },
+      { 
+        drugName: "Allegra", 
+        activeIngredient: "Fexofenadine", 
+        description: "Allegra (Fexofenadine) is an antihistamine used to relieve allergy symptoms such as watery eyes, runny nose, itching eyes/nose, sneezing, hives, and itching." 
+      },
+    ],
+    [
+      { 
+        drugName: "Nexium", 
+        activeIngredient: "Esomeprazole", 
+        description: "Nexium (Esomeprazole) is a proton pump inhibitor that decreases the amount of acid produced in the stomach." 
+      },
+      { 
+        drugName: "Prilosec", 
+        activeIngredient: "Omeprazole", 
+        description: "Prilosec (Omeprazole) is a proton pump inhibitor that decreases the amount of acid produced in the stomach." 
+      },
+      { 
+        drugName: "Prevacid", 
+        activeIngredient: "Lansoprazole", 
+        description: "Prevacid (Lansoprazole) is a proton pump inhibitor that decreases the amount of acid produced in the stomach." 
+      },
+    ],
+  ];
+  
 
 export default function Filter() {
     return (
@@ -13,11 +68,7 @@ export default function Filter() {
                 subheader="Experiment with different filters to discover different medications!"
                 className={styles.header}
             />
-            <MedicationInfoCard
-                drugName="Test"
-                activeIngredient="Still a test"
-                description="How many times do I have to say this is a test?"
-            />
+            <ResultsDisplayGrid medications={medicationsData} />
         </main>
     )
 }
