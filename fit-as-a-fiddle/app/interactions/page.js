42fc34c93fcab78782a5355e5d246bff24abd67e
elorahob <elorahob@uw.edu>
2023-08-05 04:40:29 -0700

    Add notes regarding API usage for interaction page

--- fit-as-a-fiddle/app/interactions/page.js
+++ fit-as-a-fiddle/app/interactions/page.js
@@ -10,13 +10,38 @@ const interactionData = jsonData.fullInteractionTypeGroup; // Test data containi
 
 // ALL API CALLS NEED TO HAPPEN HERE
 
+/* Selection section API call:
+  https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term=zocor&maxEntries=4
+
+  Service domain: https://rxnav.nlm.nih.gov
+  HTTP request: GET  /REST/approximateTerm.xml?term=[value]&maxEntries=[value]&option=[value]
+
+  request: https://rxnav.nlm.nih.gov/REST/approximateTerm.json?term={userInput}&maxEntries=4
+
+  ** Must filter these results to ensure that ONLY entries containing both name AND rxcui are displayed to the user. **
+
+
+  ALTERNATIVE OPTION:
+
+  
+  https://rxnav.nlm.nih.gov/REST/rxcui.json?name=Zocor
+
+  Service domain: https://rxnav.nlm.nih.gov
+  HTTP request: GET  /REST/rxcui.xml?name=yourName&allsrc=0or1&srclist=yourSources&search=0or1or2
+
+  request: https://rxnav.nlm.nih.gov/REST/rxcui.json?name={userInput}&search={0 for exact, 1 for normalized, 2 for best match}
+
+  ** This method would simply take the user input and generate card with the input as the term name and the rxcui as the API result. Would need to create component to display error if no result is found. **
+
+*/
+
 /* Results section API call:
   https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=207106+152923+656659
 
   Service domain: https://rxnav.nlm.nih.gov
   HTTP request: GET  /REST/interaction/list.xml?rxcuis=rxcuis&sources=sources
 
-  request: https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis= {['+' separated list of rxcuis in test 'drugData' array (or user-generated selectedMeds array)]}  
+  request: https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis={['+' separated list of rxcuis in test 'drugData' array (or user-generated selectedMeds array)]}&sources=DrugBank  
 */
 
 export default function Interactions() {
