662fdfbe580e6197e2f586905b470aac326b4c5e
Lily <tdong4@uw.edu>
2023-08-08 11:44:21 +0800

    added link to med cards

--- fit-as-a-fiddle/app/filter/MedicationInfoCard.jsx
+++ fit-as-a-fiddle/app/filter/MedicationInfoCard.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-const MedicationInfoCard = ({ drugName, activeIngredient, description }) => {
+const MedLinePlusBaseUrl = 'https://connect.medlineplus.gov/service';
+const MedicationInfoCard = ({ drugName, activeIngredient, description, rxcui }) => {
+  const medLinePlusUrl = `${MedLinePlusBaseUrl}?knowledgeResponseType=application%2Fjson&mainSearchCriteria.v.cs=2.16.840.1.113883.6.88&mainSearchCriteria.v.c=${rxcui}&mainSearchCriteria.v.dn=&informationRecipient.languageCode.c=en`;
+
   return (
-    <Card className="m-3" style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{drugName}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{activeIngredient}</Card.Subtitle>
-        <Card.Text>
-          {description}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <a href={medLinePlusUrl} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+      <Card className="m-3" style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{drugName}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{activeIngredient}</Card.Subtitle>
+          <Card.Text>
+            {description}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </a>
   );
 };
 
 export default MedicationInfoCard;
+
--- fit-as-a-fiddle/app/search/page.js
+++ fit-as-a-fiddle/app/search/page.js
@@ -3,64 +3,6 @@
 import styles from './page.module.css';
 import Header from '../components/Header';
 import SearchSection from './search';
-import ResultsDisplayGrid from '../filter/ResultsDisplayGrid';
-
-const medicationsData = [
-  [
-    {
-      drugName: "Aspirin",
-      activeIngredient: "Acetylsalicylic Acid",
-      description: "Aspirin is a salicylate that is used to treat pain, and reduce fever or inflammation.",
-      rxcui: 252857
-    },
-    {
-      drugName: "Tylenol",
-      activeIngredient: "Acetaminophen",
-      description: "Tylenol is used to treat mild to moderate and pain, to treat moderate to severe pain in conjunction with opiates, or to reduce fever.",
-      rxcui: 198440
-    },
-    {
-      drugName: "Advil",
-      activeIngredient: "Ibuprofen",
-      description: "Advil is a nonsteroidal anti-inflammatory drug (NSAID). It works by reducing hormones that cause inflammation and pain in the body.",
-      rxcui: 310964
-    },
-  ],
-  [
-    {
-      drugName: "Zyrtec",
-      activeIngredient: "Cetirizine",
-      description: "Zyrtec (Cetirizine) is an antihistamine used to relieve allergy symptoms such as watery eyes, runny nose, itching eyes/nose, sneezing, hives, and itching."
-    },
-    {
-      drugName: "Claritin",
-      activeIngredient: "Loratadine",
-      description: "Claritin (Loratadine) is an antihistamine used to relieve allergy symptoms such as watery eyes, runny nose, itching eyes/nose, and sneezing."
-    },
-    {
-      drugName: "Allegra",
-      activeIngredient: "Fexofenadine",
-      description: "Allegra (Fexofenadine) is an antihistamine used to relieve allergy symptoms such as watery eyes, runny nose, itching eyes/nose, sneezing, hives, and itching."
-    },
-  ],
-  [
-    {
-      drugName: "Nexium",
-      activeIngredient: "Esomeprazole",
-      description: "Nexium (Esomeprazole) is a proton pump inhibitor that decreases the amount of acid produced in the stomach."
-    },
-    {
-      drugName: "Prilosec",
-      activeIngredient: "Omeprazole",
-      description: "Prilosec (Omeprazole) is a proton pump inhibitor that decreases the amount of acid produced in the stomach."
-    },
-    {
-      drugName: "Prevacid",
-      activeIngredient: "Lansoprazole",
-      description: "Prevacid (Lansoprazole) is a proton pump inhibitor that decreases the amount of acid produced in the stomach."
-    },
-  ],
-];
 
 export default function Interactions() {
   return (
@@ -72,7 +14,6 @@ export default function Interactions() {
         className={styles.header}
       />
       <SearchSection/>
-      <ResultsDisplayGrid medications={medicationsData} />
     </main>
   )
 }
