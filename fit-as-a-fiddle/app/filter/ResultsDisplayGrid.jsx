5de15eeffdb40e3c8db52c4647a52d5b08c0262a
Lily <tdong4@uw.edu>
2023-08-08 12:05:07 +0800

    resolving conflicts

--- fit-as-a-fiddle/app/filter/ResultsDisplayGrid.jsx
+++ fit-as-a-fiddle/app/filter/ResultsDisplayGrid.jsx
@@ -1,54 +1,39 @@
-import React, { useState } from 'react';
-import MedicationInfoCard from '../search/MedicationInfoCard';
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import Pagination from 'react-bootstrap/Pagination';
+import Card from 'react-bootstrap/Card';
 
-const chunkSize = 9; // Number of cards per page
+const MedicationInfoCard = ({ drugName, manufacturer, description }) => {
+  return (
+    <Card className="m-3" style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{drugName}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{manufacturer}</Card.Subtitle>
+        <Card.Text>
+          {description}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
-const ResultsDisplayGrid = ({ medications }) => {
-  const [currentPage, setCurrentPage] = useState(0);
-
-  // Flatten the 2D array
-  const flatMedications = medications.reduce((acc, val) => acc.concat(val), []);
-
-  // Function to chunk the data into pages
-  const chunkArray = (array, size) => {
-    let result = [];
-    for (let i = 0; i < array.length; i += size) {
-      result.push(array.slice(i, i + size));
-    }
-    return result;
-  };
-
-  // Creating chunks/pages of data
-  const pages = chunkArray(flatMedications, chunkSize);
-
-  // The data for the current page
-  const currentData = pages[currentPage] || [];
-
-  let items = [];
-  for (let page = 1; page <= pages.length; page++) {
-    items.push(
-      <Pagination.Item key={page} active={page === currentPage + 1} onClick={() => setCurrentPage(page - 1)}>
-        {page}
-      </Pagination.Item>,
-    );
+const ResultsDisplayGrid = ({ medications = [] }) => {
+  if (!medications || medications.length === 0) {
+    return <p>No matching results found. Consider modifying your search for better results. </p>
   }
 
   return (
     <Container>
       <Row>
-        {currentData.map((medication, i) => (
+        {medications.map((medication, i) => (
           <Col key={`col-${i}`} xs={12} sm={6} md={4} className='mb-4'>
             <MedicationInfoCard
               drugName={medication.drugName}
-              activeIngredient={medication.activeIngredient}
+              manufacturer={medication.manufacturer}
               description={medication.description}
             />
           </Col>
         ))}
       </Row>
-      <Pagination>{items}</Pagination>
     </Container>
   );
 };
