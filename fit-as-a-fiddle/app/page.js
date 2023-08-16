5f6fecb441f65e475982438abeb6fa4b9d0b5803
Lily Dong <tdong4@uw.edu>
2023-08-16 23:59:21 +0800

    feature description appearance

--- fit-as-a-fiddle/app/page.js
+++ fit-as-a-fiddle/app/page.js
@@ -1,19 +1,10 @@
 "use client"
 
-import styles from './page.module.css'
-import 'material-icons/iconfont/material-icons.css'
+import styles from './page.module.css';
+import 'material-icons/iconfont/material-icons.css';
 import Head from 'next/head';
-import { useState } from 'react';
 
 export default function Home() {
-  const [showLearnMore, setShowLearnMore] = useState([false, false, false]);
-
-  const handleToggleLearnMore = (index, show) => {
-    const newShowLearnMore = [...showLearnMore];
-    newShowLearnMore[index] = show;
-    setShowLearnMore(newShowLearnMore);
-  };
-
   return (
     <div className={styles.home}>
       <Head>
@@ -21,7 +12,7 @@ export default function Home() {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       </Head>
       <main>
-        <section className={styles.banner}>
+      <section className={styles.banner}>
           <div className={styles.article}>
             <h1>8 reasons patients don&apos;t take their medications</h1>
             <h2>Fear, Mistrust, Worry, Misunderstanding, Too Many Medications...</h2>
@@ -37,52 +28,43 @@ export default function Home() {
             </a>
           </div>
         </section>
+
         <section className={styles.mission}>
           <h1>We make it easier for you</h1>
           <section className={styles.features}>
-            <a href="/filter" className={styles.links}>
-              <div className={styles.card}
-                  onMouseEnter={() => handleToggleLearnMore(0, true)}
-                  onMouseLeave={() => handleToggleLearnMore(0, false)}>
-                <div className={styles.name}>
-                  <img src='./images/pill.png' alt='a pill icon' className={styles.img}/>
-                  <h2>Information Filter</h2>
-                </div>
-                {showLearnMore[0] && (
-                  <div className={styles.more}>Filter to browse medications based on a specific category or drug class and various other filterable options, such as drug formula, usages, company, etc.</div>
-                )}
-              </div>
-            </a>
-
-            <a href="/search"  className={styles.links}>
-              <div
-                className={styles.card}
-                onMouseEnter={() => handleToggleLearnMore(1, true)}
-                onMouseLeave={() => handleToggleLearnMore(1, false)}>
-                <div className={styles.name}>
-                  <img src='./images/search.png' alt='a search icon' className={styles.img}/>
-                  <h2>Medication Search</h2>
-                </div>
-                {showLearnMore[1] && (
-                  <div className={styles.more}>Search and find information of a specific medication, including drug formulas, usages, potential interactions, allergy information, and much more</div>
-                )}
-              </div>
-            </a>
-
-            <a href="/interactions"  className={styles.links}>
-              <div
-                className={styles.card}
-                onMouseEnter={() => handleToggleLearnMore(2, true)}
-                onMouseLeave={() => handleToggleLearnMore(2, false)}>
+            <div className={styles.cardContainer}>
+              <a href="/filter" className={styles.links}>
+                <div className={styles.card}>
                   <div className={styles.name}>
-                    <img src='./images/check.png' alt='a shield icon' className={styles.img}/>
-                    <h2>Interaction Check</h2>
+                    <img src='./images/pill.png' alt='a pill icon' className={styles.img}/>
+                    <h2>Information Filter</h2>
                   </div>
-                  {showLearnMore[2] && (
-                      <div className={styles.more}>Check potential interactions between multiple medications. This will prevent potential harm due to a lack of knowledge of the drug interactions</div>
-                  )}
-              </div>
-            </a>
+                  <div className={styles.more}>
+                    Filter to browse medications based on a specific category or drug class and various other filterable options, such as drug formula, usages, company, etc.
+                  </div>
+                </div>
+              </a>
+
+              <a href="/search"  className={styles.links}>
+                <div className={styles.card}>
+                  <div className={styles.name}>
+                    <img src='./images/search.png' alt='a search icon' className={styles.img}/>
+                    <h2>Medication Search</h2>
+                  </div>
+                  <div className={styles.more}>Search and find information of a specific medication, including drug brand, strength, usages, allergy information, and much more!</div>
+                </div>
+              </a>
+
+              <a href="/interactions"  className={styles.links}>
+                <div className={styles.card}>
+                    <div className={styles.name}>
+                      <img src='./images/check.png' alt='a shield icon' className={styles.img}/>
+                      <h2>Interaction Check</h2>
+                    </div>
+                    <div className={styles.more}>Check potential interactions between multiple medications. This will prevent potential harm due to a lack of knowledge of the drug interactions.</div>
+                </div>
+              </a>
+            </div>
           </section>
         </section>
       </main>
